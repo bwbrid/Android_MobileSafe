54972b3d8d90df741cf3c7d3a87af05e6a186b9b
bwbrid <bwbrid@gmail.com>
2017-05-14 15:47:49 +0900

    marge the source

--- src/com/bwbrid/mobilesafe/activity/Setup1Activity.java
+++ src/com/bwbrid/mobilesafe/activity/Setup1Activity.java
@@ -1,28 +1,15 @@
 package com.bwbrid.mobilesafe.activity;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 import com.bwbrid.mobilesafe.R;
 
-public class Setup1Activity extends BaseActivity {
+public class Setup1Activity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup1);
-		
-		Button activity_setup1 = (Button) findViewById(R.layout.activity_setup1);
-		activity_setup1.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(), Setup2Activity.class);
-				startActivity(intent);
-				finish();
-			}
-		});
 	}
 }
